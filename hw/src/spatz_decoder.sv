pulp-platform/spatz

Vector Masking (#104)
* add vector masking instructions and masking support for arithmetic instructions

* add masking support for VLOAD instructions

* add support for VSTORE masking on 64bit spatz

* Solve the v0_t_reading_done issues in VFU

* add masking support for 64-bit spatz VSTORE

* add masking support for VSLDU

* add 512-bit vm support for 4096-bit register groups

* [FIX] VFU masking

* [FIX] vlsu vm_masking

* [FIX] vm_masking in vsldu

* [FIX/UPDATE] load/store tests

* [FIX] reset vl=1 before VCMP

* [TEST] uncomment mask test cases

* [FIX] masking for float comp instr

* [OPT] removing unuseful read cycle

* [FIX] rs1 val for vfslide1

* [FIX/TEST] fix masking tests for widening instr

* [HW] add masking for idx store 32b cfg

* [FIX/HW] vs2_elem_id_d trigger for idx instr

* [FIX/HW] vfu wbe generation for masking

* [FIX/HW] race btw v0 read vs. vd writeback

* [FIX/SYNTH] fixed timing loop in VFU

* [HW] doublebw VLSU masking

* [SW/FIX] VSET for masking in load/store tests

* [LINT] trailing spaces, unuseful comments

* [FIX] err in rebase VFU

* [FIX] Ventaglio/masking compatibility

---------

Co-authored-by: MaoyuanCai <472675196@qq.com>

hw/ip/spatz/src/spatz_decoder.sv
@@ -316,6 +316,14 @@ module spatz_decoder
         riscv_instr::VREDMINU_VS,
         riscv_instr::VREDMAX_VS,
         riscv_instr::VREDMAXU_VS,
+        riscv_instr::VMANDN_MM,
+        riscv_instr::VMAND_MM,
+        riscv_instr::VMOR_MM,
+        riscv_instr::VMXOR_MM,
+        riscv_instr::VMORN_MM,
+        riscv_instr::VMNAND_MM,
+        riscv_instr::VMNOR_MM,
+        riscv_instr::VMXNOR_MM,
         riscv_instr::VMSEQ_VV,
         riscv_instr::VMSEQ_VX,
         riscv_instr::VMSEQ_VI,
@@ -869,6 +877,38 @@ module spatz_decoder
               end
             end
 
+            riscv_instr::VMANDN_MM: begin
+              spatz_req.op = VMANDNOT;
+            end
+
+            riscv_instr::VMAND_MM: begin
+              spatz_req.op = VMAND;
+            end
+
+            riscv_instr::VMOR_MM: begin
+              spatz_req.op = VMOR;
+            end
+
+            riscv_instr::VMXOR_MM: begin
+              spatz_req.op = VMXOR;
+            end
+
+          riscv_instr::VMORN_MM: begin
+              spatz_req.op = VMORNOT;
+            end
+
+          riscv_instr::VMNAND_MM: begin
+              spatz_req.op = VMNAND;
+            end
+
+          riscv_instr::VMNOR_MM: begin
+              spatz_req.op = VMNOR;
+            end
+
+          riscv_instr::VMXNOR_MM: begin
+              spatz_req.op = VMXNOR;
+            end
+
             default: illegal_instr = 1'b1;
           endcase // Arithmetic Instruction Type
         end
@@ -1365,6 +1405,9 @@ module spatz_decoder
           spatz_req.op_vtl.gather_vd  = 1'b1;
           spatz_req.op_vtl.scatter_vd = 1'b1;
           spatz_req.op_vtl.idx_vreg   = vs1_field;   // explicit index vreg
+
+          // disable masking
+          spatz_req.op_arith.vm = 1'b1;
         end
 
         riscv_instr::VFXMUL_VRF: begin
@@ -1399,6 +1442,9 @@ module spatz_decoder
           spatz_req.op_vtl.scatter_vd = 1'b1;
           // op_vtl.gather_vd intentionally NOT set
           spatz_req.op_vtl.idx_vreg   = vs1_field;
+
+          // disable masking
+          spatz_req.op_arith.vm = 1'b1;
         end
 
         // vventclr — zero the entire ventaglio bank. Issued once per outer
@@ -1421,6 +1467,9 @@ module spatz_decoder
           // VTL plumbing
           spatz_req.op_vtl.use_vtl       = 1'b1;
           spatz_req.op_vtl.clear_buffer  = 1'b1;
+
+          // disable masking
+          spatz_req.op_arith.vm = 1'b1;
         end
 `endif // VENTAGLIO
 

pulp-platform/spatz_vpu

hw/src/spatz_decoder.sv
@@ -274,6 +274,14 @@ module spatz_decoder
         spatz_riscv_instr::VREDMINU_VS,
         spatz_riscv_instr::VREDMAX_VS,
         spatz_riscv_instr::VREDMAXU_VS,
+        spatz_riscv_instr::VMANDN_MM,
+        spatz_riscv_instr::VMAND_MM,
+        spatz_riscv_instr::VMOR_MM,
+        spatz_riscv_instr::VMXOR_MM,
+        spatz_riscv_instr::VMORN_MM,
+        spatz_riscv_instr::VMNAND_MM,
+        spatz_riscv_instr::VMNOR_MM,
+        spatz_riscv_instr::VMXNOR_MM,
         spatz_riscv_instr::VMSEQ_VV,
         spatz_riscv_instr::VMSEQ_VX,
         spatz_riscv_instr::VMSEQ_VI,
@@ -825,6 +833,38 @@ module spatz_decoder
               if (func3 == OPIVI) begin
                 spatz_req.rs1 = elen_t'(arith_s1);
               end
+            end
+
+            spatz_riscv_instr::VMANDN_MM: begin
+              spatz_req.op = VMANDNOT;
+            end
+
+            spatz_riscv_instr::VMAND_MM: begin
+              spatz_req.op = VMAND;
+            end
+
+            spatz_riscv_instr::VMOR_MM: begin
+              spatz_req.op = VMOR;
+            end
+
+            spatz_riscv_instr::VMXOR_MM: begin
+              spatz_req.op = VMXOR;
+            end
+
+            spatz_riscv_instr::VMORN_MM: begin
+              spatz_req.op = VMORNOT;
+            end
+
+            spatz_riscv_instr::VMNAND_MM: begin
+              spatz_req.op = VMNAND;
+            end
+
+            spatz_riscv_instr::VMNOR_MM: begin
+              spatz_req.op = VMNOR;
+            end
+
+            spatz_riscv_instr::VMXNOR_MM: begin
+              spatz_req.op = VMXNOR;
             end
 
             default: illegal_instr = 1'b1;