hazellilili/spatz

[HW] Floating-point compare instructions (#75)
* [HW] add decoding for compare instr

* [HW] mod VFU for cmp instr support

* [SW] add tests for cmp instr

* [FIX/SW] add macros for 32b config

* [OPT] single write to VRF

* [TEST] commented vmfne w sNaN

* [FIX] update LMUL in tests

* [FIX] trailing spaces

hw/ip/spatz/src/spatz_decoder.sv
@@ -855,6 +855,16 @@ module spatz_decoder
         riscv_instr::VFMIN_VF,
         riscv_instr::VFMAX_VV,
         riscv_instr::VFMAX_VF,
+        riscv_instr::VMFEQ_VV,
+        riscv_instr::VMFEQ_VF,
+        riscv_instr::VMFNE_VV,
+        riscv_instr::VMFNE_VF,
+        riscv_instr::VMFLT_VV,
+        riscv_instr::VMFLT_VF,
+        riscv_instr::VMFLE_VV,
+        riscv_instr::VMFLE_VF,
+        riscv_instr::VMFGT_VF,
+        riscv_instr::VMFGE_VF,
         riscv_instr::VFSGNJ_VV,
         riscv_instr::VFSGNJ_VF,
         riscv_instr::VFSGNJN_VV,
@@ -976,6 +986,51 @@ module spatz_decoder
               riscv_instr::VFMAX_VF: begin
                 spatz_req.op = VFMINMAX;
                 spatz_req.rm = fpnew_pkg::RTZ;
+              end
+
+              riscv_instr::VMFEQ_VV,
+              riscv_instr::VMFEQ_VF: begin
+                spatz_req.op = VFCMP;
+                spatz_req.rm = fpnew_pkg::RDN;
+              end
+
+              riscv_instr::VMFNE_VV,
+              riscv_instr::VMFNE_VF: begin
+                spatz_req.op = VFCMP;
+                spatz_req.rm = fpnew_pkg::RUP;
+              end
+
+              riscv_instr::VMFLT_VV,
+              riscv_instr::VMFLT_VF: begin
+                spatz_req.op = VFCMP;
+                spatz_req.rm = fpnew_pkg::RTZ;
+              end
+
+              riscv_instr::VMFGT_VF: begin
+                spatz_req.op = VFCMP;
+                spatz_req.rm = fpnew_pkg::RTZ;
+                //Switch the operands
+                spatz_req.vs2     = arith_s2;
+                spatz_req.use_vs2 = 1'b1;
+                spatz_req.rs1     = decoder_req_i.rs1;
+                spatz_req.use_vs1 = 1'b0;
+
+              end
+
+              riscv_instr::VMFLE_VV,
+              riscv_instr::VMFLE_VF: begin
+                spatz_req.op = VFCMP;
+                spatz_req.rm = fpnew_pkg::RNE;
+              end
+
+              riscv_instr::VMFGE_VF: begin
+                spatz_req.op = VFCMP;
+                spatz_req.rm = fpnew_pkg::RNE;
+                //Switch the operands
+                spatz_req.vs2     = arith_s2;
+                spatz_req.use_vs2 = 1'b1;
+                spatz_req.rs1     = decoder_req_i.rs1;
+                spatz_req.use_vs1 = 1'b0;
               end
 
               riscv_instr::VFMUL_VV,

hw/ip/spatz/src/spatz_fpu_sequencer.sv
@@ -419,6 +419,12 @@ module spatz_fpu_sequencer
         riscv_instr::VFSUB_VF,
         riscv_instr::VFMIN_VF,
         riscv_instr::VFMAX_VF,
+        riscv_instr::VMFEQ_VF,
+        riscv_instr::VMFNE_VF,
+        riscv_instr::VMFLT_VF,
+        riscv_instr::VMFLE_VF,
+        riscv_instr::VMFGT_VF,
+        riscv_instr::VMFGE_VF,
         riscv_instr::VFSGNJ_VF,
         riscv_instr::VFSGNJN_VF,
         riscv_instr::VFSGNJX_VF,

pulp-platform/spatz_vpu

hw/src/spatz_decoder.sv
@@ -855,6 +855,16 @@ module spatz_decoder
         spatz_riscv_instr::VFMIN_VF,
         spatz_riscv_instr::VFMAX_VV,
         spatz_riscv_instr::VFMAX_VF,
+        spatz_riscv_instr::VMFEQ_VV,
+        spatz_riscv_instr::VMFEQ_VF,
+        spatz_riscv_instr::VMFNE_VV,
+        spatz_riscv_instr::VMFNE_VF,
+        spatz_riscv_instr::VMFLT_VV,
+        spatz_riscv_instr::VMFLT_VF,
+        spatz_riscv_instr::VMFLE_VV,
+        spatz_riscv_instr::VMFLE_VF,
+        spatz_riscv_instr::VMFGT_VF,
+        spatz_riscv_instr::VMFGE_VF,
         spatz_riscv_instr::VFSGNJ_VV,
         spatz_riscv_instr::VFSGNJ_VF,
         spatz_riscv_instr::VFSGNJN_VV,
@@ -976,6 +986,51 @@ module spatz_decoder
               spatz_riscv_instr::VFMAX_VF: begin
                 spatz_req.op = VFMINMAX;
                 spatz_req.rm = fpnew_pkg::RTZ;
+              end
+
+              spatz_riscv_instr::VMFEQ_VV,
+              spatz_riscv_instr::VMFEQ_VF: begin
+                spatz_req.op = VFCMP;
+                spatz_req.rm = fpnew_pkg::RDN;
+              end
+
+              spatz_riscv_instr::VMFNE_VV,
+              spatz_riscv_instr::VMFNE_VF: begin
+                spatz_req.op = VFCMP;
+                spatz_req.rm = fpnew_pkg::RUP;
+              end
+
+              spatz_riscv_instr::VMFLT_VV,
+              spatz_riscv_instr::VMFLT_VF: begin
+                spatz_req.op = VFCMP;
+                spatz_req.rm = fpnew_pkg::RTZ;
+              end
+
+              spatz_riscv_instr::VMFGT_VF: begin
+                spatz_req.op = VFCMP;
+                spatz_req.rm = fpnew_pkg::RTZ;
+                //Switch the operands
+                spatz_req.vs2     = arith_s2;
+                spatz_req.use_vs2 = 1'b1;
+                spatz_req.rs1     = decoder_req_i.rs1;
+                spatz_req.use_vs1 = 1'b0;
+
+              end
+
+              spatz_riscv_instr::VMFLE_VV,
+              spatz_riscv_instr::VMFLE_VF: begin
+                spatz_req.op = VFCMP;
+                spatz_req.rm = fpnew_pkg::RNE;
+              end
+
+              spatz_riscv_instr::VMFGE_VF: begin
+                spatz_req.op = VFCMP;
+                spatz_req.rm = fpnew_pkg::RNE;
+                //Switch the operands
+                spatz_req.vs2     = arith_s2;
+                spatz_req.use_vs2 = 1'b1;
+                spatz_req.rs1     = decoder_req_i.rs1;
+                spatz_req.use_vs1 = 1'b0;
               end
 
               spatz_riscv_instr::VFMUL_VV,

hw/src/spatz_fpu_sequencer.sv
@@ -424,6 +424,12 @@ module spatz_fpu_sequencer
         spatz_riscv_instr::VFSUB_VF,
         spatz_riscv_instr::VFMIN_VF,
         spatz_riscv_instr::VFMAX_VF,
+        spatz_riscv_instr::VMFEQ_VF,
+        spatz_riscv_instr::VMFNE_VF,
+        spatz_riscv_instr::VMFLT_VF,
+        spatz_riscv_instr::VMFLE_VF,
+        spatz_riscv_instr::VMFGT_VF,
+        spatz_riscv_instr::VMFGE_VF,
         spatz_riscv_instr::VFSGNJ_VF,
         spatz_riscv_instr::VFSGNJN_VF,
         spatz_riscv_instr::VFSGNJX_VF,